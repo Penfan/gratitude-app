3d6137f269a56fe65c6296c6482d820fd82d729c
William Huang <william.w.huang@gmail.com>
2014-11-26 11:35:40 -0800

    user creation + seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+user = CreatePersonService.new.call
+puts 'CREATED REG USER: ' << user.email
+user = CreatePersonService.new.call
+puts 'CREATED REG USER: ' << user.email
